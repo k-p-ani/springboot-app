812cb17d42253d2f6d6475c3a3bdb92a17231f77
Ani K.P <ani.prabhakaran@gmail.com>
2022-04-21 12:11:22 +0000

    debug statement

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 echo "...buildah login....."
 # login to push image registry
+echo "...PUSH_IMAGE_REGISTRY ..... ${PUSH_IMAGE_REGISTRY}"
+echo "...PUSH_IMAGE_REGISTRY_UNAME ..... ${PUSH_IMAGE_REGISTRY_UNAME}"
+echo "...PUSH_IMAGE_REGISTRY_PWD ..... ${PUSH_IMAGE_REGISTRY_PWD}"
+
+
 buildah login -u ${PUSH_IMAGE_REGISTRY_UNAME} -p ${PUSH_IMAGE_REGISTRY_PWD} ${PUSH_IMAGE_REGISTRY}
 if [ "$?" -ne 0 ]; then
     echo "buildah login failed"
