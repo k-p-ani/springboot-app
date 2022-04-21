b08021d8a46947ef16dfb455e80ce3ba99a98016
Ani K.P <ani.prabhakaran@gmail.com>
2022-04-21 12:26:41 +0000

    debug statement

--- build-image.sh
+++ build-image.sh
@@ -6,7 +6,7 @@ echo "...PUSH_IMAGE_REGISTRY_UNAME ..... ${PUSH_IMAGE_REGISTRY_UNAME}"
 echo "...PUSH_IMAGE_REGISTRY_PWD ..... ${PUSH_IMAGE_REGISTRY_PWD}"
 
 
-buildah login -u ${PUSH_IMAGE_REGISTRY_UNAME} -p ${PUSH_IMAGE_REGISTRY_PWD} ${PUSH_IMAGE_REGISTRY}
+buildah login --tls-verify=false -u ${PUSH_IMAGE_REGISTRY_UNAME} -p ${PUSH_IMAGE_REGISTRY_PWD} ${PUSH_IMAGE_REGISTRY}
 if [ "$?" -ne 0 ]; then
     echo "buildah login failed"
 fi
